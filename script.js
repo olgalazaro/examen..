be9edd58407514b0f8bc6b8ba9ce8d9bb8d4121f
olgalazaro <lolgarely@gmail.com>
2025-04-07 21:31:04 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function saludar()
+{
+    let saludar= prompt("escribe tu nombre");
+    alert("hola"+saludar+"suerte en tu examen");
+}
+function calcularCuadrado()
+{
+    let numero=parseFloat(prompt(" escribe el numero que desea calcular "));
+    let cuadrado= (numero*numero);
+    alert ( "El cuadrado del número es" +cuadrado );
+}
